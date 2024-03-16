2960965c880e8642057296444513acb47480ea25
Shaun Thanatos Prince <suparious@solidrust.net>
2024-03-16 15:44:13 -0700

    fixing bad merge

--- nextjs-app/next.config.js
+++ nextjs-app/next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   async rewrites() {
     return [
       {
@@ -9,27 +8,10 @@ module.exports = {
     ];
   },
   exportPathMap: function() {
-=======
-  async rewrites () {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/api/:path*'
-      }
-    ]
-  },
-  exportPathMap: function () {
->>>>>>> 551e70b (refactoring)
     return {
       '/': { page: '/' },
       '/query-interface': { page: '/query-interface' },
       '/charts': { page: '/charts' }
-<<<<<<< HEAD
     };
   }
 };
-=======
-    }
-  }
-}
->>>>>>> 551e70b (refactoring)
